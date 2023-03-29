7b136f54533845d0be992fd0b36bb1b278db0ec6
T4D4 <t4d4.icp@gmail.com>
2023-03-29 23:50:00 +0900

    section4 lesson2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-// App.js
 import React, { useEffect, useState } from 'react';
 import "./App.css";
+import { ethers } from "ethers";
+import abi from "./utils/WavePortal.json";
 const App = () => {
   /* ユーザーのパブリックウォレットを保持する為に使用する状態変数を定義します */
   const [currentAccount, setCurrentAccount] = useState("");
   console.log("currentAccount", currentAccount);
+  /*
+  * デプロイされたコントラクトのアドレスを保持する変数を定義します。
+  */
+  const contractAddress = "0x85D887383274DefCa93BbeF296c648182464D8E6";
+  /*
+  * ABIの内容を参照する変数を作成します。
+  */
+  const contractABI = abi.abi;
   /* window.etherumにアクセス出来るか確認します */
   const checkIfWalletIsConnected = async () => {
     try {
@@ -28,7 +37,7 @@ const App = () => {
       console.log(error);
     }
   };
-  // connectWalletメソッドを定義します
+  // connectWalletメソッドを実装します
   const connectWallet = async () => {
     try {
       const { ethereum } = window;
@@ -44,6 +53,34 @@ const App = () => {
       console.log(error);
     }
   }
+  // waveの回数をカウントする関数を実装
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+        /* コントラクトに書き込む */
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   /*
   * WEBページがロードされた時に下記の関数を実行します。
   */
@@ -69,13 +106,19 @@ return (
           ✨
         </span>
       </div>
-      <button className="waveButton" onClick={null}>
+      {/* waveボタンにwave関数を連動させる。 */}
+      <button className="waveButton" onClick={wave}>
         Wave at Me
       </button>
       {/* ウォレットコネクトボタンの実装 */}
       {!currentAccount && (
         <button className="walletButton" onClick={connectWallet}>
-          wallet Connected
+          Connect Wallet
+        </button>
+      )}
+      {currentAccount && (
+        <button className="walletButton" onClick={connectWallet}>
+          Wallet Connected
         </button>
       )}
       </div>
